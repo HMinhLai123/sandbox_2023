f11157a2ae2d060ef7a40c4c747d6f1826d2d1a3
Lai Duc Hoang Minh <laiduchoangminh@gmail.com>
2021-03-31 13:09:40 +0800

    Add saving file feature

--- my_name.py
+++ my_name.py
@@ -3,9 +3,11 @@
 
 def main():
     greet_user()
+    save_file()
 
 def greet_user():
     print("The bat rules")
     print("dedicated to Superman, wonder woman, Aquaman, Cyborg")
-
+def save_file():
+    print("saving file.......")
 main()
